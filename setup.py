0a8e2c4619ba372c6425cdf4270c022666dc3abe
Oskari Hiltunen <felix.loether@gmail.com>
2012-09-10 09:28:00 +0300

    Make package installable.

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, Command
+import subprocess
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call(['py.test'])
+        raise SystemExit(errno)
+
+setup(
+    name='python-loaders',
+    version='0.1.0',
+    url='http://github.com/FelixLoether/python-loaders',
+    author='Oskari Hiltunen',
+    author_email='python-loaders@loethr.net',
+    description='Small collection of custom module loaders for Python.',
+    packages=['loaders'],
+    platforms='any',
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ]
+)
